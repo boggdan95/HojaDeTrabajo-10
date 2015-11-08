30cff25b26426f824b067862084c79539628747c
Bryan Chan <cha14469@uvg.edu.gt>
2015-11-08 16:46:21 -0600

    Nuevo main, lee de un archivo txt

--- src/Test.java
+++ src/Test.java
@@ -1,45 +1,59 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Vector;
-
-import org.apache.commons.collections4.map.MultiKeyMap;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Vertex ciudad1 = new Vertex("Ciudad 1");
-		Vertex ciudad2 = new Vertex("Ciudad 2");
-		Vertex ciudad3 = new Vertex("Ciudad 3");
-		Vertex ciudad4 = new Vertex("Ciudad 4");
-		Vertex ciudad5 = new Vertex("Ciudad 5");
-
-		Edge edge1 = new Edge(ciudad1, ciudad4, 10.0);
-		Edge edge2 = new Edge(ciudad1, ciudad2, 2.0);
-		Edge edge3 = new Edge(ciudad2, ciudad3, 5.0);
-		Edge edge4 = new Edge(ciudad3, ciudad4, 2.0);
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
+		BufferedReader in = new BufferedReader(new FileReader("grafo.txt"));
+		String edge;
+		String[] edgeString;
+		Vector<String> vertexNames = new Vector<String>();
+		Map<String, Vertex> vertexObjects = new HashMap<String,Vertex>();;
 		Vector<Vertex> vertices = new Vector<Vertex>();
 		Vector<Edge> edges = new Vector<Edge>();
-
-		vertices.addElement(ciudad1);
-		vertices.addElement(ciudad2);
-		vertices.addElement(ciudad3);
-		vertices.addElement(ciudad4);
-		vertices.addElement(ciudad5);
-
-		edges.addElement(edge1);
-		edges.addElement(edge2);
-		edges.addElement(edge3);
-		edges.addElement(edge4);
+		
+		while((edge = in.readLine()) != null){
+		    edgeString = edge.split(" ");
+		    if(!vertexNames.contains(edgeString[0])){
+		    	vertexNames.addElement(edgeString[0]);
+		    	vertexObjects.put(edgeString[0], new Vertex(edgeString[0]));
+		    }
+		    if(!vertexNames.contains(edgeString[1])){
+		    	vertexNames.addElement(edgeString[1]);
+		    	vertexObjects.put(edgeString[1], new Vertex(edgeString[1]));
+		    }
+		    edges.addElement(new Edge(vertexObjects.get(edgeString[0]), vertexObjects.get(edgeString[1]), Double.parseDouble(edgeString[2])));
+		}
+		in.close();
+		
+		Iterator<Entry<String, Vertex>> iterator = vertexObjects.entrySet().iterator();
+		while(iterator.hasNext()){
+			Map.Entry<String, Vertex> pair = (Entry<String, Vertex>)iterator.next();
+			vertices.addElement(pair.getValue());
+		}
 		
 		FloydWarshall floyd = new FloydWarshall(vertices, edges);
 		
 		floyd.execute();
-		Vector<Vertex> path = floyd.path(ciudad1, ciudad5);
 		
-		System.out.println("Path lenght: "+floyd.pathLenght(ciudad1, ciudad5));
+		Vertex source = vertexObjects.get("ciudad4");
+		Vertex destination = vertexObjects.get("ciudad2");
+		
+		Vector<Vertex> path = floyd.path(source, destination);
+		System.out.println("Source: "+source.getNombre()+"; Destination: "+destination.getNombre());
+		System.out.println("Path lenght: "+floyd.pathLenght(source, destination)+"km");
+		System.out.println("Path to follow:");
 		for(Vertex vertex : path){
-			System.out.println(vertex.getNombre());
+			System.out.print(vertex.getNombre()+"-->");
 		}
+		System.out.println("*");
+		
 	}
 }
