b4c2b5d3f64daf6fd20a3602c99773f45a76cee7
boggdan95 <bandabog@gmail.com>
2015-11-08 21:03:45 -0600

    Update

--- src/Test.java
+++ src/Test.java
@@ -5,11 +5,48 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Scanner;
 import java.util.Vector;
 
 public class Test {
 
+	private static int opcion;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
+	
+	int a = 1;
+	
+	while(a!=0){	
+		System.out.println("Bienvenido al mapa de Guatemala");
+        System.out.println("Seleccione lo que desea hacer \n1. Nombre de Ciudad\n2. ArrayList\n3. Lista Simplemente Enlazada\n4. Lista Doblemente Enlazada\n5. Lista Circular");
+        System.out.println("Ingrese la opción numérica deseada:");
+        
+        Scanner entradaEscaner = new Scanner (System.in); //Creación de un objeto Scanner
+        try {
+			opcion = entradaEscaner.nextInt();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			System.out.println("POR FAVOR! Ingrese un número");
+			a = 0;
+		}
+       try {
+		if (opcion > 0 && opcion <=5){
+		    	a = 1;
+		   }
+		   else{
+			   a =0;
+		   }
+		   
+			calculadoraPostfix.crearObjeto((int)entradaTeclado);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		System.out.println("POR FAVOR! Ingrese un número");
+		a = 0;
+	}
+	   }
+	}
+		
+		
 		
 		BufferedReader in = new BufferedReader(new FileReader("grafo.txt"));
 		String edge;
@@ -43,8 +80,10 @@ public class Test {
 		
 		floyd.execute();
 		
-		Vertex source = vertexObjects.get("ciudad4");
-		Vertex destination = vertexObjects.get("ciudad2");
+		
+		
+		Vertex source = vertexObjects.get("Guatemala");
+		Vertex destination = vertexObjects.get("Xela");
 		
 		Vector<Vertex> path = floyd.path(source, destination);
 		System.out.println("Source: "+source.getNombre()+"; Destination: "+destination.getNombre());
@@ -55,5 +94,8 @@ public class Test {
 		}
 		System.out.println("*");
 		
+	}
+}
+
 	}
 }
