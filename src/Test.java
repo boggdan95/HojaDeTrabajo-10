ca2c866997a7a6e1b3548b0220489db92cb41f21
boggdan95 <bandabog@gmail.com>
2015-11-08 21:29:14 -0600

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -11,10 +11,41 @@ import java.util.Vector;
 public class Test {
 
 	private static int opcion;
+	private static String ciudad1;
+	private static String ciudad2;
+	private static int a;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 	
-int a = 0;
+		BufferedReader in = new BufferedReader(new FileReader("grafo.txt"));
+		String edge;
+		String[] edgeString;
+		Vector<String> vertexNames = new Vector<String>();
+		Map<String, Vertex> vertexObjects = new HashMap<String,Vertex>();;
+		Vector<Vertex> vertices = new Vector<Vertex>();
+		Vector<Edge> edges = new Vector<Edge>();
+		
+		while((edge = in.readLine()) != null){
+		    edgeString = edge.split(" ");
+		    if(!vertexNames.contains(edgeString[0])){
+		    	vertexNames.addElement(edgeString[0]);
+		    	vertexObjects.put(edgeString[0], new Vertex(edgeString[0]));
+		    }
+		    if(!vertexNames.contains(edgeString[1])){
+		    	vertexNames.addElement(edgeString[1]);
+		    	vertexObjects.put(edgeString[1], new Vertex(edgeString[1]));
+		    }
+		    edges.addElement(new Edge(vertexObjects.get(edgeString[0]), vertexObjects.get(edgeString[1]), Double.parseDouble(edgeString[2])));
+		}
+		in.close();
+	
+		Iterator<Entry<String, Vertex>> iterator = vertexObjects.entrySet().iterator();
+		while(iterator.hasNext()){
+			Map.Entry<String, Vertex> pair = (Entry<String, Vertex>)iterator.next();
+			vertices.addElement(pair.getValue());
+		}
+				
+		a = 0;
 		
 		while(a==0){	
 			System.out.println("Bienvenido al mapa de Guatemala");
@@ -45,53 +76,42 @@ int a = 0;
 		}
 		   }
 		
+		Scanner entradaEscaner1 = new Scanner (System.in);
 		
-		BufferedReader in = new BufferedReader(new FileReader("grafo.txt"));
-		String edge;
-		String[] edgeString;
-		Vector<String> vertexNames = new Vector<String>();
-		Map<String, Vertex> vertexObjects = new HashMap<String,Vertex>();;
-		Vector<Vertex> vertices = new Vector<Vertex>();
-		Vector<Edge> edges = new Vector<Edge>();
+		try {
+			if (opcion==1){
+				System.out.println("Ingrese el nombre de la Ciudad1:");
+					ciudad1 = entradaEscaner1.next();
+				System.out.println("Ingrese el nombre de la Ciudad2:");
+					ciudad2 = entradaEscaner1.next();
+			}
+		} catch (Exception e) {
+			System.out.println("Error de escritura en la ciudad");
+			a = 0;
+		} 
 		
-		while((edge = in.readLine()) != null){
-		    edgeString = edge.split(" ");
-		    if(!vertexNames.contains(edgeString[0])){
-		    	vertexNames.addElement(edgeString[0]);
-		    	vertexObjects.put(edgeString[0], new Vertex(edgeString[0]));
-		    }
-		    if(!vertexNames.contains(edgeString[1])){
-		    	vertexNames.addElement(edgeString[1]);
-		    	vertexObjects.put(edgeString[1], new Vertex(edgeString[1]));
-		    }
-		    edges.addElement(new Edge(vertexObjects.get(edgeString[0]), vertexObjects.get(edgeString[1]), Double.parseDouble(edgeString[2])));
-		}
-		in.close();
-	
-		Iterator<Entry<String, Vertex>> iterator = vertexObjects.entrySet().iterator();
-		while(iterator.hasNext()){
-			Map.Entry<String, Vertex> pair = (Entry<String, Vertex>)iterator.next();
-			vertices.addElement(pair.getValue());
-		}
 		
 		FloydWarshall floyd = new FloydWarshall(vertices, edges);
 		
 		floyd.execute();
 		
 		
-		
-		Vertex source = vertexObjects.get("Guatemala");
-		Vertex destination = vertexObjects.get("Xela");
+		Vertex source = vertexObjects.get(ciudad1);
+		Vertex destination = vertexObjects.get(ciudad2);
 		
 		Vector<Vertex> path = floyd.path(source, destination);
-		System.out.println("Source: "+source.getNombre()+"; Destination: "+destination.getNombre());
-		System.out.println("Path lenght: "+floyd.pathLenght(source, destination)+"km");
-		System.out.println("Path to follow:");
-		for(Vertex vertex : path){
-			System.out.print(vertex.getNombre()+"-->");
+		try {
+			System.out.println("Source: "+source.getNombre()+"; Destination: "+destination.getNombre());
+			System.out.println("Path lenght: "+floyd.pathLenght(source, destination)+"km");
+			System.out.println("Path to follow:");
+			for(Vertex vertex : path){
+				System.out.print(vertex.getNombre()+"-->");
+			}
+			System.out.println("*");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Ciudades no encontradas, verifique ingresos");
+			a = 0;
 		}
-		System.out.println("*");
 		
 	}
-}
-}
